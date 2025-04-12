400a16340c41146eaadb8afee49be8a335cd436b
Luciennnnnnn <1224084650@qq.com>
2025-04-12 21:29:58 +0800

    fix multi-gpu bug

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ rawsr_ps256_bs8
 
 for((i=0;i<${#experiments[@]};i++))
 do
-    CUDA_VISIBLE_DEVICES=0 accelerate launch --multi_gpu \
+    accelerate launch --multi_gpu \
     --main_process_port 41001 \
     --num_machines 1 \
     --mixed_precision no \
